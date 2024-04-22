7c95a0ef70e0512a997ced5f7e6b226d65d46646
Paridarshan Sahoo <129207476+darshanPC25@users.noreply.github.com>
2024-04-23 01:07:17 +0530

    day 11 complete

---
+++ Day 11 - Strings in Python/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+name = "Pari"
+friend = "darshan"
+anotherFriend = 'sahoo'
+apple = '''He said, 
+Hi darshan
+hey I am good
+"I want to eat an apple'''
+ 
+print("Hello, " + name)
+# print(apple) 
+print(name[0])
+print(name[1])
+print(name[2])
