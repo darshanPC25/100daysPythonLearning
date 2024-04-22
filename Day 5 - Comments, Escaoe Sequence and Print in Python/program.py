d5f0d84cdbccea016622331ff995c73ba5007246
Paridarshan Sahoo <129207476+darshanPC25@users.noreply.github.com>
2024-04-23 00:30:35 +0530

    day 5 complete

---
+++ Day 5 - Comments, Escaoe Sequence and Print in Python/program.py
@@ -0,0 +1,6 @@
+print("I am a good boy :) \n and i'm learning python")  
+# This is the comment for leraning 
+
+#excape sequence 
+print("hello i am \"Paridarshan\" \\")
+print("Hello",6,7,8,sep="~",end="009\n\n")
